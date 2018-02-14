799be3f16e3fed9fb2f1e7dbcb739ac206fd7e2a
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-14 06:28:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-
+  
   cell = "   "
-  row = [cell, |, cell, |, cell]
+  row = [cell, "|", cell, "|", cell]
   dash = "-----------"
   row
   dash
